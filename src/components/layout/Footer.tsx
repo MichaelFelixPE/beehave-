777eb768c0c4dd2a28c155f860a10957b4748cd5
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2025-06-10 00:38:08 -0300

    Updated Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -36,7 +36,7 @@ const Footer: React.FC = () => {
             <ul className="space-y-3">
               <li className="flex items-start">
                 <MapPin size={20} className="mr-2 text-yellow-400 flex-shrink-0 mt-1" />
-                <span className="text-gray-300">Rua Bahia, N 808, Vila Moco <br />Petrolina-PE, CP 56306430</span>
+                <span className="text-gray-300">Rua Bahia, N 808, Vila Moco <br />Petrolina-PE, CEP 56306430</span>
               </li>
               <li className="flex items-center">
                 <Phone size={20} className="mr-2 text-yellow-400 flex-shrink-0" />
