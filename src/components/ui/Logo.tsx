d6ec9b5af201190957db15879aec0227f7518cf3
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2025-08-21 16:32:10 -0300

    Updated Logo.tsx

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -8,7 +8,7 @@ const Logo: React.FC<LogoProps> = ({ inverted = false }) => {
   return (
     <div className={`flex items-center justify-center w-10 h-10 rounded-full overflow-hidden ${inverted ? 'bg-yellow-400' : 'bg-yellow-400'}`}>
       <img 
-        src="https://drive.google.com/file/d/1JPh6xwOcO8u4EQnxyISmupu-qPdrBOET/view?usp=drive_link" 
+        src="https://yt3.googleusercontent.com/ytc/AIdro_ky9Qn8P9WUVR9j-g7QC4BdhwUkgP7ZP82IJs8F5EvRJcs=s900-c-k-c0x00ffffff-no-rj" 
         alt="Abelha"
         className="w-full h-full object-cover"
       />
