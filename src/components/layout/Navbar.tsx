d39e298b1612dbe4c1d9fb9a13edade2aeffc0fc
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2025-08-29 17:28:29 -0300

    Updated Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
-import beeLogo from 'public/beelogo.png';
+import beeLogo from '../../assets/beelogo.png';
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -40,12 +40,7 @@ const Navbar: React.FC = () => {
         <div className="flex justify-between h-20">
           <div className="flex-shrink-0 flex items-center">
             <a href="#" className="flex items-center" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-              <div className="flex-shrink-0 flex items-center">
-                <a href="#" className="flex items-center" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                  <img src={beeLogo} alt="Beehave Logo" className="w-10 h-10 rounded-full" />
-                  <span className="ml-2 text-xl font-bold text-black">beehave</span>
-                </a>
-              </div>
+              <img src="/beelogo.png" alt="Beehave Logo" className="w-10 h-10 rounded-full" />
               <span className="ml-2 text-xl font-bold text-black">   beehave </span>
             </a>
           </div>
