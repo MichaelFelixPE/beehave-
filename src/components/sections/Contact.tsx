e04c552da18ccccfb9d3998952e21c084e115143
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2025-08-31 01:14:58 -0300

    Updated Contact.tsx

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -103,7 +103,7 @@ const Contact: React.FC = () => {
                   <h4 className="text-lg font-medium text-black">Horário de Funcionamento</h4>
                   <p className="text-gray-700 mt-1">
                     Segunda - Sexta: 08:00 - 18:00<br />
-                    Sábado: 9:00 - 13:00<br />
+                    Sábado: 09:00 - 13:00<br />
                     Domingo: Fechado
                   </p>
                 </div>
