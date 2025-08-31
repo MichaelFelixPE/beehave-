8162badd82e772cab03e04f7a9d4612faf733612
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2025-08-31 01:35:04 -0300

    Updated Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar: React.FC = () => {
           <div className="flex-shrink-0 flex items-center">
             <a href="#" className="flex items-center" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
               <img src="https://i.imgur.com/KukZ26T.png" alt="Beehave Logo" className="w-11 h-11" />
-              <span className="ml-2 text-xl font-bold text-black">   beehave: Soluções Comportamentais </span>
+              <span className="ml-2 text-xl font-bold text-black"> beehave: Soluções Comportamentais </span>
             </a>
           </div>
           
