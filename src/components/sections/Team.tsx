42f0fd496acd2e99aa3117452f558b2e1976e666
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2026-01-13 13:29:49 -0300

    Updated Team.tsx

--- src/components/sections/Team.tsx
+++ src/components/sections/Team.tsx
@@ -328,7 +328,7 @@ const teamHierarchy: TeamMember[] = [
           },
           {
             name: 'Patrícia Matos',
-            image: 'https://via.placeholder.com/200x200/FCD34D/1F2937?text=PM',
+            image: 'https://i.imgur.com/TxZAhzt.jpeg',
             level: 'E-EVAL',
             crm: 'CRP 00/00000',
             formations: [
