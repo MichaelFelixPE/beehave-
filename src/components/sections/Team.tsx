d0806a622ef824cb86f952c5b1784f3e93ed9cd9
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2026-01-13 22:25:29 -0300

    Updated Team.tsx

--- src/components/sections/Team.tsx
+++ src/components/sections/Team.tsx
@@ -202,7 +202,7 @@ const teamHierarchy: TeamMember[] = [
             crm: 'CRP-02/26606',
             formations: [
               'Graduação em Psicologia (Universidade Federal do Vale do São Francisco – UNIVASF)',
-              'Especialização em Análise do Comportamento Aplicada (ABA) para TEA e Deficiência Intelectual (CBI of Miami)'
+              'Especialização em Análise do Comportamento Aplicada (ABA) para TEA e Deficiência Intelectual (CBI of Miami)',
               'Formação em Gerenciamento Profissional de Crises – Praticante Nível 2',
             ],
             bio: 'Coordenadora especializada em atendimento clínico e supervisão de casos.',
