bb4f419479c9aeeb5f0a3e618e876b3e73db4a51
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2026-01-27 19:03:05 -0300

    Updated Team.tsx

--- src/components/sections/Team.tsx
+++ src/components/sections/Team.tsx
@@ -260,7 +260,7 @@ const teamHierarchy: TeamMember[] = [
               'Graduação em Psicologia',
               'Pós-graduação em Análise do Comportamento Aplicada (ABA)',
               'Pós-graduação em andamento em Neuropsicologia com ênfase em avaliações',
-              'Graduação em Psicologia'
+              'Capacitação em Protocolos de Avaliação do Desenvolvimento e do Comportamento'
               
             ],
             bio: 'Me chamo Carlane Machado, sou psicóloga, Coordenadora ABA e Coordenadora Técnica. Sou pós-graduada em Análise do Comportamento Aplicada (ABA) e pós-graduanda em Neuropsicologia com ênfase em avaliações. Possuo capacitação na aplicação de protocolos de avaliação do desenvolvimento e do comportamento, utilizados na construção e acompanhamento do Plano Terapêutico Individualizado.Atuo na avaliação e intervenção de crianças com atrasos no desenvolvimento e transtornos do neurodesenvolvimento, 7 anos de experiência na área. Trabalhando com crianças e adolescentes, típicos e atípicos, sempre com foco em práticas baseadas em evidências, tomada de decisão clínica e atuação interdisciplinar.',
