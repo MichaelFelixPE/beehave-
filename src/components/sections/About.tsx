bd98a55b11901e3943fe773fca0d06d1f3d8d8c6
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2026-02-11 17:50:29 -0300

    Updated About.tsx

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -12,7 +12,7 @@ const About: React.FC = () => {
   const images = {
     missao: "https://i.imgur.com/8eC7zWT.jpeg",
     visao: "https://i.imgur.com/tpV4ayQ.jpeg",
-    valores: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&q=80"
+    valores: "https://i.imgur.com/PYMsM7a.jpeg"
   };
 
   return (
