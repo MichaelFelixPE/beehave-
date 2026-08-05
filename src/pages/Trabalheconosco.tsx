fd5c5b091863f087d5b52c1780720dbe6f201d75
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2026-08-05 15:52:37 -0300

    Updated Trabalheconosco.tsx

---
+++ src/pages/Trabalheconosco.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TrabalheConosco from '../components/sections/TrabalheConosco';
+
+const TrabalhePage: React.FC = () => {
+  return <TrabalheConosco />;
+};
+
+export default TrabalhePage;
