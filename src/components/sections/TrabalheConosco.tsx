f7fac69036f1bcc4f31798fbfce5a04b69fcfd74
Michael Felix <143030160+MichaelFelixPE@users.noreply.github.com>
2026-09-08 19:55:22 -0300

    Updated TrabalheConosco.tsx

--- src/components/sections/TrabalheConosco.tsx
+++ src/components/sections/TrabalheConosco.tsx
@@ -56,7 +56,7 @@ const TrabalheConosco: React.FC = () => {
           <p className="mt-4 text-gray-700 max-w-xl mx-auto">
             Quer fazer parte da BeeEquipe? Envie seu currículo e nos conte sobre você.
             <br />
-            Buscamos profissionais comprometidos com o Afeto, a Ciência e a Ética.
+            Buscamos profissionais comprometidos com Afeto, Ciência e a Ética.
           </p>
         </div>
 
